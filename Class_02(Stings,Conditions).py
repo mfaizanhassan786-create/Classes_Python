e8d1a4d41991c3ca500984246451653da866c9b7
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-13 14:44:51 +0500

    New Strings and Conditions File

---
+++ Class_02(Stings,Conditions).py
@@ -0,0 +1,140 @@
+#String operations:
+
+str1="Faizan"
+str2="Hassan"
+print(str1+str2)
+print(str1+" "+str2)
+print(str1+" "*5+str2)
+print(str1+" "*5+str2+" "*5+str1)
+print(str1+" "*5+str2+" "*5+str1+" "*5+str2+" "*5+str1)
+
+#Another Example:
+str1= "This is a string."
+str2='This is a string.'
+str3="""This is a string."""
+print(str1)
+print(str2)
+print(str3)
+
+#Escape Sequence:
+str1="This is a string.\n we are using the Escape sequence."
+print(str1)
+
+#Another Example:
+str2="This is a string.\t we are using the Escape sequence."
+print(str2)
+
+#Another Example:
+str3="This is a string.\r we are using the Escape sequence."
+print(str3)
+
+#Concatenation:
+str1="Faizan"
+str2="Hassan"
+print(str1+str2)
+
+# Another Example:
+str1="Python Programming"
+str2="We are learning Python"
+print(str1+" "+str2)
+
+#Length of String:
+
+str1 = "Python"
+len1 = len(str1)
+print(len1)
+str2 = "Programming"
+len2 = len(str2)
+print(len2)
+final_str = str1 + " " + str2
+print(final_str)
+print(len(final_str))
+print(len1+len2)
+print(len1-len2)
+print(len1*len2)
+print(len1/len2)
+print(len1%len2)
+print(len1**len2)
+print(len1//len2)
+
+#String Slicing or Indexing:
+
+
+str="123456789"
+print(str[5])
+print(str[0:3])
+print(str[0:3:1])
+print(str[0:3:-1])
+print(str[0:3:-2])
+print(str[0:3:-3])
+print(str[0:3:-4])
+print(str[0:3:-5])
+print(str[0:3:-6])
+print(str[0:3:-7])
+print(str[0:3:-8])
+print(str[0:3:-9])
+
+#Another Example:
+str1="Welcome to Python"
+ch=str1[0]
+print(ch)
+str2=str1[0:3]
+print(str2)
+str3=str1[0:3:1]
+print(str3)
+
+#Another Example:
+str1 = "Python"
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+print(str1[5])
+
+
+#Another Example:
+str1 = "Python"
+print(str1[0:3])
+print(str1[1:4])
+print(str1[2:5])
+print(str1[3:6])
+print(str1[4:7])
+print(str1[5:8])
+print(str1[6:9])
+
+#Another Example:
+str1 = "Hello Python"
+print(str1[0:3])
+print(str1[1:4])
+print(str1[2:5])
+print(str1[3:6])
+print(str1[4:7])
+print(str1[5:8])
+print(str1[6:9])
+
+#Another Example:
+str1 = "Programming"
+print(str1[0:3])
+print(str1[1:4])
+print(str1[2:5])
+print(str1[3:6])
+print(str1[4:7])
+print(str1[5:8])
+print(str1[6:9])
+
+
+classes_01 = "Python"
+print(classes_01[1:len(classes_01)])
+
+# String Slicing:
+
+str1 = "Python"
+print(str1[-5:-1])
+print(str1[-5:-2])
+print(str1[-5:-3])
+print(str1[-5:-4])
+print(str1[-5:-5])
+print(str1[-5:-6])
+print(str1[-5:-7])
+ 
