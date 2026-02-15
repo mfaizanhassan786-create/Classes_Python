f3f2922f26b33230ffde7d4f9a71d94d6afafb81
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-15 21:51:46 +0500

    Update File

--- Class_06(Sets).py
+++ Class_06(Sets).py
@@ -1,15 +1,34 @@
 #set is the collection of unordered, immutable and unindexed elements.
 
 
-Collection = {1, 2, 3, 4, 5}
-print(Collection)
-print(type(Collection))
-print(len(Collection))
-print(max(Collection))
-print(min(Collection))
+# Collection = {1, 2, 3, 4, 5}
+# print(Collection)
+# print(type(Collection))
+# print(len(Collection))
+# print(max(Collection))
+# print(min(Collection))
 # print(sum(Collection))
 # print(sorted(Collection))
-# print(reversed(Collection))
-# print(Collection.add(6))
-# print(Collection.remove(6))
-# print(Collection.discard(6))
+
+
+
+# collection = {"Hello", "World", "Python", "Java", "C++"}
+# print(collection)
+# print(type(collection))
+
+#What is the empty set?
+
+
+
+# Collection = set()
+
+#Some are the elements in the sets.
+
+#Add element:
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+print(collection)
+
