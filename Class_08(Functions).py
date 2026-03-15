5a9fe96aadbc240683f6c92a54585338cd56019d
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-03-15 09:03:57 +0500

    update

---
+++ Class_08(Functions).py
@@ -0,0 +1,31 @@
+#Functions in the Python.
+# What is a Function?
+# "A function is a reusable block of organized code designed to perform a single,
+# related action. It only runs when it is "called" by its name."
+# Real Python Tutorials
+# Real Python Tutorials
+#  +2
+# Why use it:
+# Reusability: Write once and use many times without rewriting the same code.
+# Organization: Breaks large programs into smaller, manageable, and logical parts.
+# Clarity: Improves readability by using descriptive names for tasks (e.g.,
+
+#Some are the examples:
+
+# def greet():
+#     """A simple function that prints a greeting."""
+#     print("Hello!")
+
+# greet()  # Call the function
+
+# def sum(a, b):
+#     s = a + b
+#     print(s)
+#     return s
+
+def sum(c, b):
+    c =10
+    b =30
+    s = c + b
+    print(s)
+    return s
